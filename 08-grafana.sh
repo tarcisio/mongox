9c0eb8268814d8c82bbc1b4e9d973d908895b0ae
Tarcísio Ferreira Sassara <sassara.tarcisio@gmail.com>
2021-06-24 19:45:34 -0300

    Update 08-grafana.sh

--- 08-grafana.sh
+++ 08-grafana.sh
@@ -2,3 +2,4 @@
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm install grafana grafana/grafana
+kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-np
