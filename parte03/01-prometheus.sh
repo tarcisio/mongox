4c40f214a9c3f2f75454f3904afb827ceea0474a
tarcisio <tarcisio.sassara@gmail.com>
2021-06-25 00:48:28 -0300

    f

--- parte03/01-prometheus.sh
+++ parte03/01-prometheus.sh
@@ -2,4 +2,5 @@
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/prometheus
-kubectl expose service prometheus-server --type=NodePort --port=9090 --target-port=9090 --name=prometheus-server-np
+kubectl expose service prometheus-server --type=NodePort --port=3111 --target-port=9090 --name=prometheus-server-np
+
